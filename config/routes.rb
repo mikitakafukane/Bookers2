8793764d5b1e4bed1a21deea5abf4e6c6da3a33a
EC2 Default User <ec2-user@ip-172-31-63-56.ec2.internal>
2021-03-02 10:24:43 +0000

    カリキュラムのコメント機能作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,11 @@ Rails.application.routes.draw do
   
   root to: 'homes#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :books
+  resources :books do
+    resources:book_comments,only:[:create, :destroy]
+  end
+  
   resources :users
   get "home/about" => "homes#about", as: "about"
+  
 end
-
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_16_095128) do
+ActiveRecord::Schema.define(version: 2021_03_02_093859) do
+
+  create_table "book_comments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "book_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "comment"
+  end
 
   create_table "books", force: :cascade do |t|
     t.datetime "created_at", null: false
