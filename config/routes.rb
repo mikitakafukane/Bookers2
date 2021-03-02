8bd1bffbe3341ea399e1548fc1caaaaede055d3e
EC2 Default User <ec2-user@ip-172-31-63-56.ec2.internal>
2021-03-02 14:27:04 +0000

    応用課題３完了

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :books do
     resources:book_comments,only:[:create, :destroy]
+    resource:favorites, only:[:create, :destroy]
   end
   
   resources :users
