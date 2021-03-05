629240a72eee5b528b1675b335e48906a3d879ed
EC2 Default User <ec2-user@ip-172-31-63-56.ec2.internal>
2021-03-05 08:00:04 +0000

    前方一致、後方一致修正

--- app/controllers/searchs_controller.rb
+++ app/controllers/searchs_controller.rb
@@ -5,21 +5,29 @@ class SearchsController < ApplicationController
     @content = params["content"]
     @records = search_for(@model,@content,@method)
   end
-  
+
   private
-  
+
   def search_for(model, content, method)
     if model == 'user'
       if method == 'perfect'
         User.where(name: content)
+      elsif method == 'forward_match'
+        User.where('name LIKE ?', content+'%')
+      elsif method == 'backward_match'
+        User.where('name LIKE ?','%'+content)
       else
         User.where('name LIKE ?','%'+content+'%')
       end
     elsif model =='book'
       if method == 'perfect'
         Book.where(title: content)
+      elsif method == 'forward_match'
+        Book.where('name LIKE ?', content+'%')
+      elsif method == 'backward_match'
+        Book.where('name LIKE ?','%'+content)
       else
-        Book.where('title LIKE ?', '%'+content+'%')
+        Book.where('name LIKE ?','%'+content+'%')
       end
     end
   end
